6fcc4677424da8231a5590ea01cff2ebf4a838d6
Dhruv Rathi <dhruvrathi15@gmail.com>
2017-01-21 21:13:45 +0530

    Added cloud backend

--- app/src/main/java/com/stonecode/chargemonitor/BeaconDetectActivity.java
+++ app/src/main/java/com/stonecode/chargemonitor/BeaconDetectActivity.java
@@ -1,15 +1,9 @@
 package com.stonecode.chargemonitor;
 
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.os.Bundle;
 import android.os.RemoteException;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -21,7 +15,9 @@ import org.altbeacon.beacon.MonitorNotifier;
 import org.altbeacon.beacon.Region;
 import org.altbeacon.beacon.powersave.BackgroundPowerSaver;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.HashMap;
 
 public class BeaconDetectActivity extends AppCompatActivity implements BeaconConsumer {
@@ -55,11 +51,15 @@ public class BeaconDetectActivity extends AppCompatActivity implements BeaconCon
 
             }
 
+            long recordTime[] = new long[8];
+
             @Override
             public void didDetermineStateForRegion(int i, Region region) {
                 String regionName = region.getUniqueId();
                 String beaconSSN = region.getId2().toHexString();
-                Log.d(TAG, "didDetermineStateForRegion: " + i+regionName+beaconSSN);
+                int x = returnId(regionName);
+
+                Log.d(TAG, "didDetermineStateForRegion: " + i + regionName + beaconSSN);
                 switch (i) {
                     case INSIDE:
                         Log.i("TAG", "Enter " + regionName);
@@ -67,6 +67,15 @@ public class BeaconDetectActivity extends AppCompatActivity implements BeaconCon
                             regionNames.add(regionName);
                             regions.add(beaconSSN);
                             changeData();
+
+                            Log.d(TAG, "!region");
+
+                            Calendar c = Calendar.getInstance();
+                            long currTime = c.getTimeInMillis();
+
+                            BeaconToParse data = new BeaconToParse(x, 1, String.valueOf(currTime - recordTime[x]));
+                            recordTime[x] = currTime;
+                            data.sendToParse();
                         }
 
                         break;
@@ -78,11 +87,16 @@ public class BeaconDetectActivity extends AppCompatActivity implements BeaconCon
                             changeData();
                         }
 
+                        Log.d(TAG, "X:::" + x);
+                        BeaconToParse data = new BeaconToParse(x, 0, "0");
+                        recordTime[x] = 0;
+                        data.sendToParse();
+
                         break;
                 }
             }
-
         });
+
         try {
             for (String key : ssnRegionMap.keySet()) {
                 Region region = ssnRegionMap.get(key);
@@ -91,6 +105,40 @@ public class BeaconDetectActivity extends AppCompatActivity implements BeaconCon
         } catch (RemoteException e) {
             e.printStackTrace();
         }
+    }
+
+    public int returnId(String appl) {
+
+        int i = 1;
+        if (appl.equals("Air Conditioner")) {
+            i = 1;
+        }
+
+        if (appl.equals("Smart Meter")) {
+            i = 2;
+        }
+
+        if (appl.equals("Fridge")) {
+            i = 3;
+        }
+
+        if (appl.equals("Lighting")) {
+            i = 4;
+        }
+
+        if (appl.equals("Washing Machine")) {
+            i = 5;
+        }
+
+        if (appl.equals("Television")) {
+            i = 6;
+        }
+
+        if (appl.equals("Heater")) {
+            i = 7;
+        }
+
+        return i;
     }
 
     public void changeData() {
@@ -113,18 +161,16 @@ public class BeaconDetectActivity extends AppCompatActivity implements BeaconCon
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_beacon_detect);
 
-
         ssnRegionMap = new HashMap<>();
-
 
         ssnRegionMap.put("0x000000000001", new Region("Air Conditioner", Identifier.parse("0x424531b6b1bc0a2fa89e"), Identifier.parse("0x000000000001"), null));
         ssnRegionMap.put("0x111122223333", new Region("Smart Meter", Identifier.parse("0x11112222333344445555"), Identifier.parse("0x111122223333"), null));
         ssnRegionMap.put("0x000000000002", new Region("Fridge", Identifier.parse("0xb8ed0f35da2d8a65c6ef"), Identifier.parse("0x000000000002"), null));
-        ssnRegionMap.put("0x000000000003", new Region("Lighting",Identifier.parse("0x9a932f9cebea0d781574"), Identifier.parse("0x000000000003"), null));
-        ssnRegionMap.put("0x000000000004", new Region("Washing Machine", Identifier.parse("0xf805e1875ba2df7b1359"),Identifier.parse("0x000000000004"), null));
-        ssnRegionMap.put("0x000000000005", new Region("Television", Identifier.parse("0xdcdc608daaada6179199"),  Identifier.parse("0x000000000005"), null));
-        ssnRegionMap.put("0x000000000006", new Region("Heater", Identifier.parse("0x911181170acb61798c0b"),  Identifier.parse("0x000000000006"), null));
-        ssnRegionMap.put("0x000000000007", new Region("Geyser", Identifier.parse("0xc662d18ebceda1eca8be"),  Identifier.parse("0x000000000007"), null));
+        ssnRegionMap.put("0x000000000003", new Region("Lighting", Identifier.parse("0x9a932f9cebea0d781574"), Identifier.parse("0x000000000003"), null));
+        ssnRegionMap.put("0x000000000004", new Region("Washing Machine", Identifier.parse("0xf805e1875ba2df7b1359"), Identifier.parse("0x000000000004"), null));
+        ssnRegionMap.put("0x000000000005", new Region("Television", Identifier.parse("0xdcdc608daaada6179199"), Identifier.parse("0x000000000005"), null));
+        ssnRegionMap.put("0x000000000006", new Region("Heater", Identifier.parse("0x911181170acb61798c0b"), Identifier.parse("0x000000000006"), null));
+        ssnRegionMap.put("0x000000000007", new Region("Geyser", Identifier.parse("0xc662d18ebceda1eca8be"), Identifier.parse("0x000000000007"), null));
 
         mBeaconManager = BeaconManager.getInstanceForApplication(this.getApplicationContext());
 
@@ -132,10 +178,14 @@ public class BeaconDetectActivity extends AppCompatActivity implements BeaconCon
                 setBeaconLayout(BeaconParser.EDDYSTONE_UID_LAYOUT));
         new BackgroundPowerSaver(this);
 
+        Calendar c = Calendar.getInstance();
+        Log.d(TAG, "currTime:  " + c.getTime());
+
+        SimpleDateFormat df = new SimpleDateFormat("dd-MMM-yyyy");
+        String formattedDate = df.format(c.getTime());
+        Log.d(TAG, "formattedDate: " + formattedDate);
         lvDepartment = (ListView) findViewById(R.id.lv_Departments);
         departmentAdapter = new ArrayAdapter<String>(getApplicationContext(), R.layout.list_item_department, regionNames);
         lvDepartment.setAdapter(departmentAdapter);
-
-            }
-
+    }
 }
---
+++ app/src/main/java/com/stonecode/chargemonitor/BeaconToParse.java
@@ -0,0 +1,78 @@
+package com.stonecode.chargemonitor;
+
+import android.util.Log;
+
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+import static android.content.ContentValues.TAG;
+
+/**
+ * Created by dhruv on 21/1/17.
+ */
+
+public class BeaconToParse {
+
+    int appliance_id;
+    int deviceStatus;
+    String timeStatus;
+    String appliance;
+
+    // send the appliance id according to below
+
+    public BeaconToParse(int appliance_id, int deviceStatus, String timeStatus) {
+        this.appliance_id = appliance_id;
+        this.timeStatus = timeStatus;
+        this.deviceStatus = deviceStatus;
+//        sendToParse();
+    }
+
+    public void sendToParse() {
+
+        final ParseUser currentUser = ParseUser.getCurrentUser();
+        switch (appliance_id) {
+            case 1:
+                appliance = "Airconditioner";
+                break;
+            case 2:
+                appliance = "SmartMeter";
+                break;
+            case 3:
+                appliance = "Fridge";
+                break;
+            case 4:
+                appliance = "Lighting";
+                break;
+            case 5:
+                appliance = "WashingMachine";
+                break;
+            case 6:
+                appliance = "TV";
+                break;
+            case 7:
+                appliance = "Heater";
+                break;
+        }
+
+        Log.d("beasonToPrse", appliance);
+        Log.d("beacon", currentUser.toString());
+
+        currentUser.put(appliance, String.valueOf(deviceStatus));
+        currentUser.put("deviceStatus", String.valueOf(deviceStatus));
+        currentUser.put("timeStatus", timeStatus);
+
+        currentUser.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if(e!=null)
+                {
+                    e.printStackTrace();
+//                    Log.d("errorin",e.printStackTrace());
+                }
+//                Log.d(TAG, "e::: " + e);
+//                Log.d(TAG,"error:"+currentUser.get("Fridge").toString());
+            }
+        });
+    }
+}
